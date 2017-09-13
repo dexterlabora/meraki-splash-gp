f517ee1d4b223a2dd0f3928fb7b0d6bcf9937a62
Cory Guynn <cory@premiereportal.net>
2017-09-13 21:02:31 +0200

    heroku commit

---
+++ configs.sample.js
@@ -0,0 +1,15 @@
+//configs.js
+// Rename this file to configs.js 
+// Define your Application Configurations here
+var config = {
+    // Meraki API Key
+    apiKey: "YourAPIKey", 
+    // The "shard" number for your Meraki organization (find this in your dashboard URL)
+    shard: "n###", 
+    // The Meraki Network ID
+    networkId: "YourNetworkID", 
+    // The Meraki Group Policy ID 
+    policy: "102"
+}
+
+module.exports = config;
---
+++ routes/login.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var request = require("request");
+var configs = require("./../configs");
+
+/* POST login data. */
+router.post('/', function(req, res, next) {
+    console.log("login.js / req.body: ",req.body);
+
+    // bind client to a group policy id
+    bindGroupPolicy(req.body.client_mac, configs.policy);
+
+    // Store Data
+    // ToDo write to a database 
+    var session = req.body;
+    session.policy = configs.policy;
+    console.log("Client Session",session);
+
+
+    // Process Meraki Login
+    res.writeHead(302, {
+      'Location': req.body.base_grant_url + "?continue_url="+req.body.user_continue_url}
+    );
+    res.end();
+      //res.render('index', payload);
+});
+
+// Meraki API - Bind Client to Group Policy
+function bindGroupPolicy(clientMac, groupPolicyId) {
+  var options = { method: 'PUT',
+    url: 'https://'+configs.shard+'.meraki.com/api/v0/networks/'+configs.networkId+'/clients/'+clientMac+'/policy',
+    qs: { timespan: '84000' },
+    headers: 
+     { 'content-type': 'application/json',
+       'x-cisco-meraki-api-key': configs.apiKey},
+    body: { devicePolicy: 'Group policy', groupPolicyId: groupPolicyId },
+    json: true };
+  
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  
+    console.log(body);
+  });
+}
+  
+
+module.exports = router;
+
