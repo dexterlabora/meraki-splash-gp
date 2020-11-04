da0d8f569d597bc3a383197f8143555b35147749
Stefan Zdravkov <szdravkov93@gmail.com>
2020-11-04 16:58:17 +0100

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,15 +1,16 @@
 //configs.js
-// Rename this file to configs.js 
+// Rename this file to configs.js
 // Define your Application Configurations here
 var config = {
-    // Meraki API Key
-    apiKey: "2f301bccd61b6c642d250cd3f76e5eb66ebd170f", // Demo API Key
-    // Meraki API Base URL
-    apiUrl: "https://api.meraki.com/api/v0", 
-    // The Meraki Network ID
-    networkId: "L_643451796760560141", 
-    // The Meraki Group Policy ID 
-    policy: "103"
-}
+  // Meraki API Key
+  apiKey: "2f301bccd61b6c642d250cd3f76e5eb66ebd170f", // Demo API Key
+  //   apiKey: "ef351f4d132afe99b69dd179cd2d51e8ea285156", // test API Key
+  // Meraki API Base URL
+  apiUrl: "https://api.meraki.com/api/v0",
+  // The Meraki Network ID
+  networkId: "L_643451796760560141",
+  // The Meraki Group Policy ID
+  policy: "103",
+};
 
 module.exports = config;
